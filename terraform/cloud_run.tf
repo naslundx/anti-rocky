00505e1baa3bfb37ad89ac2fb7e996281fb94ea9
Johan Niklasson <johan@niklasson.me>
2025-10-04 21:36:27 +0200

    Allocating latest revision

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -7,12 +7,25 @@ resource "google_cloud_run_v2_service" "api" {
   template {
     containers {
       image = "europe-west1-docker.pkg.dev/${var.gcp_project}/${var.gcp_project}/api:latest"
+      resources {
+        limits = {
+          "cpu"    = "1"
+          "memory" = "256Mi"
+        }
+        startup_cpu_boost = true
+      }
       env {
         name  = "env"
         value = "production"
       }
     }
   }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+
   lifecycle {
     ignore_changes = [
       template[0].containers[0].env,
