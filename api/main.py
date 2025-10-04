db06e1443685ee7619021829f057427829c2f411
Johan Niklasson <johan@niklasson.me>
2025-10-04 17:16:59 +0200

    Added comments

--- api/main.py
+++ api/main.py
@@ -6,17 +6,20 @@ from flask_cors import CORS
 from clients.sbdb import SBDBClient
 from clients.neo import NeoClient
 
+# Setup Clients and API Keys
+
 env = os.environ.get("env", "development").lower()
 neo_api_key = os.environ.get("neo_api_key", None)
 
 neo_client = NeoClient(neo_api_key)
 sbdb_client = SBDBClient()
 
-app = Flask(__name__)
+# Setup app and Cors
 
+app = Flask(__name__)
 CORS(app, origins=["http://www.defending.earth", "https://www.defending.earth"])
 
-
+# Redirect to www if we're in prod
 @app.route("/", methods=["GET"])
 def index():
     if env == "production":
@@ -26,12 +29,12 @@ def index():
 
     return "Hello Space", 200
 
-
+# List objects
 @app.route("/objects/", methods=["GET"])
 def list_objects():
     return neo_client.list(), 200
 
-
+# Detailed object
 @app.route("/objects/<neo_id>/", methods=["GET"])
 def get_object(neo_id: str):
     neo_data = sbdb_client.get(neo_id)
